80ec6c3d646c37b55d3ea05fcfe47ae56d120b28
Arthur Leywin <110176533+kamatashi@users.noreply.github.com>
2022-10-03 12:50:30 -0300

    Update periodos.js

--- periodos.js
+++ periodos.js
@@ -1,4 +1,3 @@
-//lista de materias
 const materias = [{
   anoLetivo: 2017.1,
   codigo: "ADM0002",
@@ -30,75 +29,71 @@ const materias = [{
   nota: 4,
 }]
 
-// Funções auxiliares
-const soma = (y, x) => y + x
-const somatorio = (valores) => valores.reduce(soma)
-const ehInteiro = numero => divisor => {
-  // Caso base
-  if (numero == 0) return 0
-  else if (numero == 1) return 1
-  else if (numero == divisor) return 1
-  else if (numero < divisor) return 'O divisor é maior que o numerador. O resultado não seria um número inteiro.'
-  else return ehInteiro(numero)(divisor + 1)
+
+module.exports = function bigPower(){
+
+  // Funções auxiliares
+  const soma = (y, x) => y + x
+  const somatorio = (valores) => valores.reduce(soma)
+  const ehInteiro = numero => divisor => {
+    // Caso base
+    if (numero == 0) return 0
+    else if (numero == 1) return 1
+    else if (numero == divisor) return 1
+    else if (numero < divisor) return 'O divisor é maior que o numerador. O resultado não seria um número inteiro.'
+    else return ehInteiro(numero)(divisor + 1)
+  }
+  const decimal = (lista) => lista.map(ehInteiro)
+  const filtroNotas = (lista) => lista.map((x) => x.nota)
+  const media = (lista) => somatorio(lista) / lista.length
+  const ifelse = (lista) => lista.filter((x) => Number.isInteger(x))
+  const alteraSituacao = (lista) => lista.map(item => item.codigo)
+  
+  
+  
+  //função que calcula média aritmética ponderada  
+  const mediaPonderada = (lista) => somatorio(lista.map((x) => x.nota * x.CH)) / lista.length
+  //console.log("A média ponderada entre suas notas e carga horária é " + (mediaPonderada(materias)) + "\n")
+  
+  
+  //função que calcula o desvio-padrão   
+  const desvioPadrao = (materias) => {
+    const desvioPadrao = (lista) => lista.map((x) => ((x - media(filtroNotas(materias))) ** 2 / lista.length))
+    const desvioParcial = desvioPadrao(filtroNotas(materias))
+    return Math.sqrt(somatorio(desvioParcial))
+  }
+  
+  
+  
+  //aprovado e reprovado
+  const aprovado = (lista) => lista.filter((x) => x.nota >= 5)
+  const reprovado = (lista) => lista.filter((x) => x.nota < 5)
+  
+  
+  //carga horaria total cursada
+  const somar = (lista) => lista.reduce(soma, 0)
+  const cargaHoraria = (lista) => lista.map((item) => item.CH)
+  const todaCargaHoraria = somar(cargaHoraria(materias))
+  
+  
+  const selecDepartamento = (codigo='') => (lista) => mediaPonderada(lista.filter((x)=> x.codigo.replace(/[0-9]/g,'') == codigo))
+  
+  
+  //função de seleção e média ponderada por departamento
+  
+  //quantos periodos a pessoa fez
+  const filtroPeriodo = (lista) => lista.map((x) => x.anoLetivo)
+  const organizar = (lista) => lista.sort((a, b) => (a - b))
+  const ultimo = (lista) => lista[lista.length - 1]
+  const primeiro = (lista) => lista[0]
+  const ano = ((ultimo(organizar(filtroPeriodo(materias))) - primeiro(organizar(filtroPeriodo(materias)))))
+  const periodo = Math.trunc(((ultimo(organizar) - primeiro(organizar)) + 0.4) * 2)
+  
+  
+  // Logs (Visualização na prática)
+  console.log(`Média Ponderada: ${mediaPonderada(materias)}
+    Aprovado em    : ${alteraSituacao(aprovado(materias))}
+    Reprovado em   : ${alteraSituacao(reprovado(materias))}
+    Carga horária  : ${todaCargaHoraria} horas
+    Desvio Padrão  : ${desvioPadrao(materias)}`)
 }
-const decimal = (lista) => lista.map(ehInteiro)
-const filtroNotas = (lista) => lista.map((x) => x.nota)
-const media = (lista) => somatorio(lista) / lista.length
-const ifelse = (lista) => lista.filter((x) => Number.isInteger(x))
-const alteraSituacao = (lista) => lista.map(item => item.codigo)
-
-
-
-//função que calcula média aritmética ponderada  
-const mediaPonderada = (lista) => somatorio(lista.map((x) => x.nota * x.CH)) / lista.length
-//console.log("A média ponderada entre suas notas e carga horária é " + (mediaPonderada(materias)) + "\n")
-
-
-//função que calcula o desvio-padrão   
-const desvioPadrao = () => {
-  const desvioPadrao = (lista) => lista.map((x) => ((x - media(filtroNotas(materias))) ** 2 / lista.length))
-  const desvioParcial = desvioPadrao(filtroNotas(materias))
-  return Math.sqrt(somatorio(desvioParcial))
-
-}
-
-
-
-//aprovado e reprovado
-const aprovado = (lista) => lista.filter((x) => x.nota >= 5)
-const reprovado = (lista) => lista.filter((x) => x.nota < 5)
-
-
-//carga horaria total cursada
-const somar = (lista) => lista.reduce(soma, 0)
-const cargaHoraria = (lista) => lista.map((item) => item.CH)
-const todaCargaHoraria = somar(cargaHoraria(materias))
-
-
-
-
-//função de seleção e média ponderada por departamento
-
-//quantos periodos a pessoa fez
-const filtroPeriodo = (lista) => lista.map((x) => x.anoLetivo)
-const organizar = (lista) => lista.sort((a, b) => (a - b))
-const ultimo = (lista) => lista[lista.length - 1]
-const primeiro = (lista) => lista[0]
-const ano = ((ultimo(organizar(filtroPeriodo(materias))) - primeiro(organizar(filtroPeriodo(materias)))))
-const comDecimal = (ano + 0.4).toFixed(1)
-const semDecimal = ano
-
-
-
-const periodo = ifelse * 2
-
-//console.log(ano)
-//console.log(periodo)
-
-
-// Logs (Visualização na prática)
-console.log(`Média Ponderada: ${mediaPonderada(materias)}
-Aprovado em    : ${alteraSituacao(aprovado(materias))}
-Reprovado em   : ${alteraSituacao(reprovado(materias))}
-Carga horária  : ${todaCargaHoraria} horas
-Desvio Padrão  : ${desvioPadrao(materias)}`)
